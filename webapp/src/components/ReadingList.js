f7131ba88f34cf8f62c4c87e3eaff426597ebf6b
Shariq.x <memonshariq10@gmail.com>
2024-10-05 01:06:48 +0500

    creating other page

---
+++ webapp/src/components/ReadingList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ReadingList() {
+  return (
+    <div className="reading-list">
+      <h2>My Reading List</h2>
+      {/* Sample list */}
+      <ul>
+        <li>Book 1</li>
+        <li>Book 2</li>
+        <li>Book 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ReadingList;
